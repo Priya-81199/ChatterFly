7566ee1505591defb41bcce78a83638039e75c5e
Priya-81199 <priya81199@gmail.com>
2020-10-26 23:39:05 +0530

    Changed Startup Page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class ChatterFly extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: WelcomeScreen.id,
+      initialRoute: LoginScreen.id,
       routes: {
         WelcomeScreen.id : (context) => WelcomeScreen(),
         LoginScreen.id : (context) => LoginScreen(),
